0f27ad640f4baead972c459f9af7cb6d195b703e
Adrian Chudzynski <adrian-chudzynsk@straightvisions.com>
2019-07-24 17:00:15 +0200

    Removed obsolete page templates

--- page-sidebar.php
+++
@@ -1,7 +0,0 @@
-<?php
-	/*
-	Template Name: Page with Sidebar
-	Template Post Type: page
-	*/
-	
-	echo $GLOBALS['sv100']->get_module( 'sv_content' ) ? $GLOBALS['sv100']->get_module( 'sv_content' )->load( array( 'template' => 'page_sidebar' ) ) : '';
